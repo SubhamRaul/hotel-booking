bd6c22f0573598fa96e0e1026dd424538a09ade8
Subham Raul <subhamraul52@gmail.com>
2025-12-06 01:41:05 +0530

    added HotelRooms page , problem in radio button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar'
 import {Route, Routes, useLocation} from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Footer from './components/Footer.jsx';
+import Allroom from './pages/Allroom.jsx';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<Allroom />} />
         </Routes>
       </div>
       <Footer/>
